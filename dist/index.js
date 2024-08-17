fa8dc7d642794c56be251fd6b4e01e619065ba21
Richie77777777 <richeanyadike7@gmail.com>
2024-08-17 23:15:47 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// index.ts
+const Car_1 = require("./Car");
+// Create an instance of Car
+const myCar = new Car_1.Car('Toyota', 'Corolla', 2020);
+// Call the start method
+myCar.start();
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// index.ts
+import { Car } from './Car';
+
+// Create an instance of Car
+const myCar = new Car('Toyota', 'Corolla', 2020);
+
+// Call the start method
+myCar.start();
